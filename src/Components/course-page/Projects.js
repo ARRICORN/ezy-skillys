bd1ec5de5b9d4c973d88a22b41171202511fa765
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-05-15 16:46:27 +0600

    Added projects components (basic)

--- src/Components/course-page/Projects.js
+++ src/Components/course-page/Projects.js
@@ -3,7 +3,10 @@ import { Fragment } from "react";
 const Projects = () => {
     return (
         <Fragment>
-            
+                   <div className="flex items-center">
+                <h2 className="heading w-3/4 lg:w-1/4">Angular JS Projects</h2>
+                <div className="w-1/2 lg:w-full border border-[#FF8401] h-0 relative -top-[5px]"></div>
+                </div>
         </Fragment>
     );
 };
