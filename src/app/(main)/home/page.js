130364d9913585e252ba8d9f8bec7f30bb2b7c4e
syedalamin <syedalamin2580@gmail.com>
2024-05-17 19:12:52 +0800

    World’s

--- src/app/(main)/home/page.js
+++ src/app/(main)/home/page.js
@@ -1,9 +1,11 @@
 import SkillYourWay from "@/Components/Home/SkillYourWay";
+import WorldsFirstAI from "@/Components/Home/WorldsFirstAI";
 
 const HomePage = () => {
   return (
-    <div className='w-11/12 mx-auto'>
+    <div className='w-11/12 mx-auto space-y-20'>
       <SkillYourWay />
+      <WorldsFirstAI/>
     </div>
   );
 };
