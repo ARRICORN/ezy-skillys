3ac20d5b8a9192529ae68038672985c80ea44610
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-05-28 11:33:21 +0600

    Updated pricing layout

--- src/Components/pricing/MainContainer.js
+++ src/Components/pricing/MainContainer.js
@@ -1,18 +1,21 @@
 import Image from "next/image";
-import beads from "/src/assets/beads1.png"
+import beads from "/src/assets/beads2.png";
+import wave from "/src/assets/Wave.svg"
 
 const MainContainer = () => {
-    return (
-        <div className="min-h-screen">
-        <div className="waveBg min-h-[50vh] bg-[#003F7D] rounded-bl-full flex justify-around">
-                <Image src={beads} alt="beads"/>
-                <div>
-
-                </div>
-                <Image src={beads} alt="beads"/>
-        </div>
-        </div>
-    );
+  return (
+    <div className="min-h-screen">
+      <div className={` bg-[#003F7D] flex justify-around min-h-[80vh] items-end`}>
+        <Image src={beads} alt="beads" className="w-20 h-36 z-10" />
+        <div></div>
+        <Image src={beads} alt="beads" className="w-20 h-36 z-10" />
+        
+      </div>
+      <div>
+        <Image src={wave} alt="wave" className="w-full relative -top-32"/>
+     </div>
+    </div>
+  );
 };
 
 export default MainContainer;
