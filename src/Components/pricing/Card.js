0879e5aeb108409e79ed5b0c727cbe05ab4b6d59
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-05-28 23:35:14 +0600

    Updated cards

--- src/Components/pricing/Card.js
+++ src/Components/pricing/Card.js
@@ -1,23 +1,27 @@
+import Image from "next/image";
 import { Fragment } from "react";
 
-const Card = ({programType, price, perks}) => {
-    return (
-        <Fragment>
-            <div className="flex flex-col">
-                <div>
-                    <span>{programType}</span>
-                </div>
-                <div>
-                    {price} + Taxes
-                </div>
-                <div>
-                    <p>{perks.first}</p>
-                    <p>{perks.second}</p>
-                </div>
-                <button>Choose Plan</button>
+const Card = (props) => {
+  const { programType, price, perks } = props;
+  return (
+    <Fragment>
+      <div className="flex flex-col z-50">
+        <div>
+          <span>{programType}</span>
+        </div>
+        <div>{price} + Taxes</div>
+        <div>
+          {Array.isArray(perks) && perks.map((each) => (
+            <div key={each?.id}>
+              <Image src={each?.img} alt={each?.text} />
+              <span>{each?.text}</span>
             </div>
-        </Fragment>
-    );
+          ))}
+        </div>
+        <button>Choose Plan</button>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Card;
