253160a0bc9daae942d079f0f7727b1f67d685e3
Robin <mohammadrobin636@gmail.com>
2024-05-29 23:22:30 +0600

    Fixed a bug

--- src/app/api/courses/getCourse/[courseID]/route.js
+++ src/app/api/courses/getCourse/[courseID]/route.js
@@ -1,18 +1,9 @@
 import { Course } from "@/Models/Course";
-import { UserInfo } from "@/Models/UserInfo";
-import checkIsLoggedIn from "@/middlewares/checkIsLoggedIn";
 import mongoose from "mongoose";
 
 export async function GET(req, { params }) {
   try {
-    // Checking the user is logged in or not by checking the token;
-    const decoded = checkIsLoggedIn();
     mongoose.connect(process.env.DATABASE_URL);
-
-    const userInfo = await UserInfo.findOne({ email: decoded.email });
-    if (!userInfo) {
-      throw new Error("You are not authorized!");
-    }
 
     const courseID = params.courseID;
     const result = await Course.findById(courseID);
@@ -20,7 +11,7 @@ export async function GET(req, { params }) {
       throw new Error("Course not found!");
     }
     if (result.isDeleted) {
-      throw new Error("The course is deleted!");
+      throw new Error("This course is deleted!");
     }
     return Response.json({
       success: true,
