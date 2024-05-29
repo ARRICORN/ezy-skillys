7177a367f293b91f86c7235eb9d351fda5b96aee
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-05-29 18:33:29 +0600

    Added pricing component

--- src/Components/pricing/Card.js
+++ src/Components/pricing/Card.js
@@ -5,20 +5,34 @@ const Card = (props) => {
   const { programType, price, perks } = props;
   return (
     <Fragment>
-      <div className="flex flex-col z-50">
-        <div>
-          <span>{programType}</span>
+      <div className="flex flex-col z-50 items-center w-[15.5rem] relative rounded-xl">
+        <div className="w-3/5 text-center bg-white absolute -top-5 py-2 px-3 rounded-md leading-none text-sm font-semibold">
+          <span className="text-[#FF8B36] text-center">{programType}</span>
         </div>
-        <div>{price} + Taxes</div>
-        <div>
-          {Array.isArray(perks) && perks.map((each) => (
-            <div key={each?.id}>
-              <Image src={each?.img} alt={each?.text} />
-              <span>{each?.text}</span>
-            </div>
-          ))}
+        <div className="bg-[#FF8B36] text-white w-full h-28 text-center p-6 flex items-end rounded-t-xl">
+          <p>
+            <span className=" text-4xl font-bold">{price}</span> + Taxes
+          </p>
         </div>
-        <button>Choose Plan</button>
+        <div className="w-full flex flex-col text-sm p-6 bg-white h-32 gap-2">
+          {Array.isArray(perks) &&
+            perks.map((each) => (
+              <div
+                key={each?.id}
+                className="flex bg-white justify-center items-center text-xs gap-3"
+              >
+                <div className="w-1/5">
+                  <Image src={each?.img} alt={each?.text} className="w-full" />
+                </div>
+                <span className="w-5/6">{each?.text}</span>
+              </div>
+            ))}
+        </div>
+        <div className="w-full bg-white flex justify-center rounded-b-xl shadow-md pt-4 pb-10">
+          <button className="w-1/2 border-[1.5px] py-2 px-3 border-[#FF8B36] rounded-md text-sm text-[#FF8B36]">
+            Choose Plan
+          </button>
+        </div>
       </div>
     </Fragment>
   );
--- src/Components/pricing/MainContainer.js
+++ src/Components/pricing/MainContainer.js
@@ -3,16 +3,16 @@ import beads from "/src/assets/beads2.png";
 import wave from "/src/assets/Wave.svg";
 import { Fragment } from "react";
 import Card from "./Card";
-import individuals from "/src/assets/individuals.png"
-import calender from "/src/assets/calender.png"
-import colleges from "/src/assets/college.png"
+import individuals from "/src/assets/individuals.png";
+import calender from "/src/assets/calender.png";
+import colleges from "/src/assets/college.png";
 
 const MainContainer = () => {
   const programDetails = [
     {
       id: 1,
       programType: "College Program",
-      price: "$100/-",
+      price: "$100",
       perks: [
         {
           id: 1,
@@ -29,7 +29,7 @@ const MainContainer = () => {
     {
       id: 2,
       programType: "Employee Program",
-      price: "$250/-",
+      price: "$250",
       perks: [
         {
           id: 1,
@@ -46,7 +46,7 @@ const MainContainer = () => {
     {
       id: 3,
       programType: "Complete Transformation Program",
-      price: "$400/-",
+      price: "$400",
       perks: [
         {
           id: 1,
@@ -64,20 +64,20 @@ const MainContainer = () => {
   return (
     <Fragment>
       <div
-        className={` bg-[#003F7D] min-h-[80vh] flex flex-col justify-between pt-10 md:pt-12 lg:pt-24`}
+        className={` bg-[#003F7D] min-h-[100vh] flex flex-col justify-between pt-10 md:pt-12 lg:pt-24`}
       >
         <h1 className="text-3xl text-center font-bold text-white">
           Our <span className="text-[#FF8B36]">Pricing</span>
         </h1>
         <div className="flex justify-around items-end w-full">
           <Image src={beads} alt="beads" className="w-20 h-36 z-10" />
-          <div className="flex justify-center z-50">
+          <div className="flex justify-center z-50 gap-10">
             {programDetails.map((each) => (
               <div key={each.id}>
                 <Card
-                    programType= {each.programType}
-                    price= {each.price}
-                    perks= {each.perks}
+                  programType={each.programType}
+                  price={each.price}
+                  perks={each.perks}
                 />
               </div>
             ))}
@@ -89,7 +89,7 @@ const MainContainer = () => {
         <Image
           src={wave}
           alt="wave"
-          className="w-full relative -top-12 md:-top-20 lg:-top-32"
+          className="w-full relative -top-12 md:-top-20 lg:-top-24"
         />
       </div>
     </Fragment>
