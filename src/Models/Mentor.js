2ba73544df30a5b3ddb464f96b08d9652738c800
Thejellyfish1024 <alamin102410@gmail.com>
2024-06-23 12:41:52 +0600

    made the get method for mentors

--- src/Models/Mentor.js
+++ src/Models/Mentor.js
@@ -3,9 +3,11 @@ import mongoose, { Schema } from "mongoose";
 const MentorSchema = new Schema(
     {
         name: { type: String, required: true },
+        email: { type: String, required: true },
+        number: { type: String },
         description: { type: String },
         image: { type: String },
-        students: { type: [String] , default: [] }, // array will contain all the ids of trained students 
+        students: { type: [String], default: [] }, // array will contain all the ids of trained students 
     },
     { timestamps: true }
 );
--- src/app/api/mentors/route.js
+++ src/app/api/mentors/route.js
@@ -2,6 +2,37 @@ import mongoose from "mongoose";
 import { Mentor } from "@/Models/Mentor";
 import { isAdmin } from "@/middlewares/checkAdmin";
 
+// Get method for new mentors in the descending order for students array length
+export const GET = async (req) => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+        const result = await Mentor?.aggregate([
+            {
+                $addFields: {
+                    studentsLength: { $size: "$students" }
+                }
+            },
+            {
+                $sort: { studentsLength: -1 }
+            }
+        ]);
+        return new Response(JSON.stringify(
+            {
+                status: "Success",
+                data: result,
+            }),
+            { status: 201 }
+        );
+    } catch (error) {
+        return new Response(JSON.stringify(
+            {
+                message: "Error in getting the mentors",
+            }),
+            { status: 500 }
+        );
+    }
+};
+
 // Create method for new mentors
 export const POST = async (req) => {
     try {
