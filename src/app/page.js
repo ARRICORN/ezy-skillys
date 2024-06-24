9f7dcfebdc77f5c24a147c178215e4c7e733e3d5
syedalamin <syedalamin2580@gmail.com>
2024-06-25 00:25:47 +0600

    user icon and animation toggle

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,8 @@
-import SkillDevelopment from "@/Components/Home/SkillDevelopment";
-import SkillYourWay from "@/Components/Home/SkillYourWay";
-import WorldsFirstAI from "@/Components/Home/WorldsFirstAI";
 
 export default function Home() {
   return (
     <main>
-      <div className="w-11/12 mx-auto space-y-32">
-        <SkillYourWay />
-        <WorldsFirstAI />
-        <SkillDevelopment />
-      </div>
+     Hello 
     </main>
   );
 }
