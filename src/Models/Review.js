89a27cc9b27c30649f7d2ecae43acd01a459b5ac
Robin <mohammadrobin636@gmail.com>
2024-06-24 12:14:11 +0600

    Fixed a bug in creating course and completed adding reviews to purchased courses

---
+++ src/Models/Review.js
@@ -0,0 +1,17 @@
+
+
+import mongoose, { Schema } from "mongoose";
+import { Course } from "./Course";
+
+const ReviewSchema = new Schema(
+  {
+    courseId: { type: mongoose.Types.ObjectId, required: true, ref: Course },
+    rating: { type: Number, required: true, },
+    review: { type: String, required: true },
+    addedBy: { type: String, required: true },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const Review =
+  mongoose.models.Review || mongoose.model("Review", ReviewSchema);
