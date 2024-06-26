11bf9706ccabd6322739ab000b1a5e1aff173993
Robin <mohammadrobin636@gmail.com>
2024-06-26 14:15:36 +0600

    Completed adding sorting and pagination on the get courses API

--- src/app/api/courses/myPurchasedCourses/route.js
+++ src/app/api/courses/myPurchasedCourses/route.js
@@ -3,7 +3,7 @@ import { UserInfo } from "@/Models/UserInfo";
 import checkIsLoggedIn from "@/middlewares/checkIsLoggedIn";
 import mongoose from "mongoose";
 
-export async function GET() {
+export async function GET(req) {
   try {
     // Checking the user is logged in or not by checking the token;
     const decoded = checkIsLoggedIn();
@@ -16,13 +16,35 @@ export async function GET() {
     if (userInfo.role !== "user") {
       throw new Error("Only user can see their purchased courses!");
     }
+
+    const url = new URL(req.url);
+
+    const sortingType = url.searchParams.get("sort") || "ascending";
+    const sort = sortingType === "descending" ? -1 : 1;
+    const pageValue = url.searchParams.get("page") || 1;
+    const limitValue = url.searchParams.get("limit") || 10;
+    const page = Number(pageValue);
+    const limit = Number(limitValue);
+    const skip = (Number(page) - 1) * limit;
+
     const result = await PurchasedCourse.find({
       userEmail: userInfo.email,
-    }).populate("course").select("-createdAt -updatedAt -__v");
+    })
+      .sort({ price: sort })
+      .skip(skip)
+      .limit(limit)
+      .populate("course")
+      .select("-createdAt -updatedAt -__v");
 
     return Response.json({
       success: true,
       message: "My purchased courses are retrieved successfully",
+      meta: {
+        page,
+        limit,
+        skipped: skip,
+        totalData: result.length,
+      },
       data: result,
     });
   } catch (error) {
