3ef7e463128216b93d0e89f1ab1c471bb18305c5
monzurul002 <monzurul002@gmail.com>
2024-06-27 00:05:07 +0600

    dynamic route added

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['s3-alpha-sig.figma.com', "w7.pngwing.com", "seeklogo.com", "i.ibb.co", "upload.wikimedia.org"],
-    },
+  images: {
+    domains: [
+      "s3-alpha-sig.figma.com",
+      "w7.pngwing.com",
+      "seeklogo.com",
+      "i.ibb.co",
+      "upload.wikimedia.org",
+      "i.ibb.co",
+      "res.cloudinary.com",
+    ],
+  },
 };
 
 export default nextConfig;
