9472c28c51bb8f29ff250947c2527c89bead0ba8
Hriday-paul <hridoychandrapaul.10@gmail.com>
2024-06-29 18:53:10 +0600

    faq task is complete

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,5 +1,6 @@
 import Footer from "@/Components/Shared/Footer";
 import NavBar from "@/Components/Shared/NavBar/NavBar";
+import NextUi_Provider from "@/Components/Shared/NextUiProvider";
 import ClientScrollToTop from "@/Components/Ui/ScrollToTop";
 
 import NextAuthSessionProvider from "@/Provider/SessionProvider";
@@ -10,16 +11,18 @@ import { Fragment } from "react";
 export default function MainLayout({ children }) {
   return (
     <main >
-      
-      <NavBar/>
+
+      <NavBar />
       <div className=" container mx-auto">
-    
-      <NextAuthSessionProvider>
-     
-        <div className="min-h-screen">
-          {children}
-        </div>
-      </NextAuthSessionProvider>
+
+        <NextAuthSessionProvider>
+
+          <div className="min-h-screen">
+            <NextUi_Provider>
+              {children}
+            </NextUi_Provider>
+          </div>
+        </NextAuthSessionProvider>
       </div>
       <ClientScrollToTop />
       <Footer />
