9383128a15e15320db97ef30d66f4dc22150b8e4
Mohammad Hidayat Ullah Reza <91150098+Mohammad-Hidayat-Ullah-Reza@users.noreply.github.com>
2024-07-02 19:11:10 +0600

    1. Stripe implimented. 2. Payment page created. 3. payment success page created. 3. stripe-payment-intent api route created. 4. In .env file hidayat's public and secret stripe key added.

---
+++ src/Components/Checkout/CheckoutForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckoutForm = ({ price = 4000 }) => {
+  return <div>Course price {price}</div>;
+};
+
+export default CheckoutForm;
---
+++ src/app/(main)/checkout/page.js
@@ -0,0 +1,12 @@
+import CheckoutForm from "@/Components/Checkout/CheckoutForm";
+import React from "react";
+
+const Checkout = () => {
+  return (
+    <div>
+      <CheckoutForm />
+    </div>
+  );
+};
+
+export default Checkout;
