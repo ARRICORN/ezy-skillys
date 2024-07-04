16763e6a35864935b4518a1ff6d4ae8cb0a4ab1f
Mohammad Hidayat Ullah Reza <91150098+Mohammad-Hidayat-Ullah-Reza@users.noreply.github.com>
2024-07-04 23:02:40 +0600

    courses page course card style changed

--- src/Components/courses/AllCourses.js
+++ src/Components/courses/AllCourses.js
@@ -12,47 +12,47 @@ const AllCourses = ({ data, setModalStatus, setPdfLink }) => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center py-12 gap-y-28 lg:gap-x-10 px-6 lg:gap-y-32 sm:gap-x-8">
           {data?.data?.length > 0 ? (
             data?.data?.map((each) => (
-
               <div
                 key={each._id}
                 className="bg-[#003F7D] h-[300px] w-full flex flex-col rounded-2xl items-center p-4 box-border cols-span-1"
               >
-
-                <div className="w-full h-full object-cover rounded-xl">
+                <div className="w-full h-full rounded-xl">
                   <Link href={`/courses/${each._id}`}>
                     <Image
                       src={each.image}
                       alt={`${each.title} image`}
                       width={120}
                       height={120}
-                      className="h-[140px] w-full rounded-xl"
+                      className="h-[140px] object-contain w-full rounded-xl"
                     />
                   </Link>
                 </div>
 
-
                 <div className="bg-white text-center py-4 px-2 rounded-2xl mt-4 box-border flex flex-col w-full">
-                  <Link href={`/courses/${each._id}`}>
-                    <h2 className="mb-2 font-semibold text-xl">{each.title}</h2>
-                    <p className="text-sm text-center word-wrap mb-4">
-                      {each.desc.length > 100
-                        ? `${each.desc.slice(0, 100)}...`
-                        : each.desc}
-                    </p>
-                    <div className="flex justify-center gap-2">
-                      <button className="px-3 py-2 text-[10px] border-[#F98149] rounded-xl border flex gap-1 items-center">
-                        <Image className="w-3" src={monitor} alt="Monitor" />{" "}
-                        <span>Live Demo</span>
-                      </button>
-                      <Link
+                  {/* <Link href={`/courses/${each._id}`}> */}
+                  <h2 className="mb-2 font-semibold text-xl">{each.title}</h2>
+                  <p className="text-sm text-center word-wrap mb-4">
+                    {each.desc.length > 100
+                      ? `${each.desc.slice(0, 100)}...`
+                      : each.desc}
+                  </p>
+                  <div className="flex justify-center gap-2">
+                    <Link
+                      href={`/courses/${each._id}`}
+                      className="px-3 py-2 w-full flex items-center justify-center text-[10px] border-primary rounded-xl border text-white text-base font-semibold mx-5 gap-1 mt-4 bg-primary hover:bg-opacity-95"
+                    >
+                      {/* <Image className="w-3" src={monitor} alt="Monitor" />{" "} */}
+                      <span>See Details</span>
+                    </Link>
+                    {/* <Link
                         href={`/payment/${each._id}`}
                         className="px-3 py-2 text-[10px] border-[#F98149] rounded-xl border flex gap-1 items-center"
                       >
                         <Image className="w-3" src={pin} alt="Pin" />
                         <span>Enroll Now</span>
-                      </Link>
-                    </div>
-                    <button
+                      </Link> */}
+                  </div>
+                  {/* <button
                       onClick={() => {
                         setPdfLink(
                           "https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_100KB_PDF.pdf"
@@ -63,13 +63,10 @@ const AllCourses = ({ data, setModalStatus, setPdfLink }) => {
                     >
                       <Image className="w-4" src={download} alt="Download" />
                       <span>Download Curriculum</span>
-                    </button>
-                  </Link>
+                    </button> */}
+                  {/* </Link> */}
                 </div>
-
-
               </div>
-
             ))
           ) : (
             <div className="col-span-4 h-[300px] flex items-center justify-center">
