6c24fb8ab2065bde0b55a55edbe7ee622060d12b
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2024-07-14 15:16:08 +0600

    test

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -29,19 +29,81 @@
 // };
 
 // export default page;
-import dynamic from 'next/dynamic';
-import React, { Fragment } from "react";
+// import dynamic from 'next/dynamic';
+// import React, { Fragment } from "react";
 
-// Dynamic imports for all components
-const SkillDevelopment = dynamic(() => import("@/Components/Home/SkillDevelopment"), { ssr: false });
-const SkillYourWay = dynamic(() => import("@/Components/Home/SkillYourWay"), { ssr: false });
-const WorldsFirstAI = dynamic(() => import("@/Components/Home/WorldsFirstAI"), { ssr: false });
-const Achievement = dynamic(() => import("@/Components/achivepages/achievements/Achievement"), { ssr: false });
-const CertificateAchievement = dynamic(() => import("@/Components/achivepages/certificate-achivement/CertificateAchievement"), { ssr: false });
-const MentorsTrainer = dynamic(() => import("@/Components/achivepages/mentors-trainers-review/MentorsTrainer"), { ssr: false });
-const PopularCourses = dynamic(() => import("@/Components/achivepages/popular-courses/PopularCourses"), { ssr: false });
-const Project_planning = dynamic(() => import("@/Components/achivepages/project-planning/Project_planning"), { ssr: false });
-const HomeReview = dynamic(() => import("@/Components/Home/HomeReview"), { ssr: false });
+// // Dynamic imports for all components
+// const SkillDevelopment = dynamic(() => import("@/Components/Home/SkillDevelopment"), { ssr: false });
+// const SkillYourWay = dynamic(() => import("@/Components/Home/SkillYourWay"), { ssr: false });
+// const WorldsFirstAI = dynamic(() => import("@/Components/Home/WorldsFirstAI"), { ssr: false });
+// const Achievement = dynamic(() => import("@/Components/achivepages/achievements/Achievement"), { ssr: false });
+// const CertificateAchievement = dynamic(() => import("@/Components/achivepages/certificate-achivement/CertificateAchievement"), { ssr: false });
+// const MentorsTrainer = dynamic(() => import("@/Components/achivepages/mentors-trainers-review/MentorsTrainer"), { ssr: false });
+// const PopularCourses = dynamic(() => import("@/Components/achivepages/popular-courses/PopularCourses"), { ssr: false });
+// const Project_planning = dynamic(() => import("@/Components/achivepages/project-planning/Project_planning"), { ssr: false });
+// const HomeReview = dynamic(() => import("@/Components/Home/HomeReview"), { ssr: false });
+
+// const Page = () => {
+//   return (
+//     <Fragment>
+//       <div className="w-11/12 mx-auto space-y-32">
+//         <SkillYourWay />
+//         <WorldsFirstAI />
+//         <SkillDevelopment />
+//         {/* How it works section */}
+//         <Project_planning />
+//         <PopularCourses />
+//         <Achievement />
+//         <MentorsTrainer />
+//         <HomeReview />
+//         <CertificateAchievement />
+//       </div>
+//     </Fragment>
+//   );
+// };
+
+// export default Page;
+
+import { Fragment } from "react";
+import dynamic from "next/dynamic";
+
+const SkillDevelopment = dynamic(
+  () => import("@/Components/Home/SkillDevelopment"),
+  { ssr: false }
+);
+const SkillYourWay = dynamic(() => import("@/Components/Home/SkillYourWay"), {
+  ssr: false,
+});
+const WorldsFirstAI = dynamic(() => import("@/Components/Home/WorldsFirstAI"), {
+  ssr: false,
+});
+const Achievement = dynamic(
+  () => import("@/Components/achivepages/achievements/Achievement"),
+  { ssr: false }
+);
+const CertificateAchievement = dynamic(
+  () =>
+    import(
+      "@/Components/achivepages/certificate-achivement/CertificateAchievement"
+    ),
+  { ssr: false }
+);
+const MentorsTrainer = dynamic(
+  () =>
+    import("@/Components/achivepages/mentors-trainers-review/MentorsTrainer"),
+  { ssr: false }
+);
+const PopularCourses = dynamic(
+  () => import("@/Components/achivepages/popular-courses/PopularCourses"),
+  { ssr: false }
+);
+const Project_planning = dynamic(
+  () => import("@/Components/achivepages/project-planning/Project_planning"),
+  { ssr: false }
+);
+const HomeReview = dynamic(() => import("@/Components/Home/HomeReview"), {
+  ssr: false,
+});
 
 const Page = () => {
   return (
@@ -50,7 +112,6 @@ const Page = () => {
         <SkillYourWay />
         <WorldsFirstAI />
         <SkillDevelopment />
-        {/* How it works section */}
         <Project_planning />
         <PopularCourses />
         <Achievement />
